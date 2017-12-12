6a0e90bcc6c05e7384721b44c4069d58b66b24d6
nemtomki <34485128+nemtomki@users.noreply.github.com>
2017-12-12 17:54:30 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void lengyel ()
+{
+// Peti, kapok .txt fájlt, aminél a példa legyen:
+// ezt leforditom lengyel formává:
+
+}
+
+void regiszteres ()
+{
+//Áron kapja a lengyel formát:
+//leforditja regiszteresre:
+}
+
+
+void gepikod ()
+
+{
+//Lcai a kapott gépi kódot, kiszámolja:
+
+//eredmény , adott példákkal:
+}
+
+
+int main()
+{
+
+    return 0;
+}
