46a9756d35f22132ce5f875c03aa0a745fc415a0
nemtomki <34485128+nemtomki@users.noreply.github.com>
2017-12-12 18:17:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 void lengyel ()
 {
-// Peti, kapok .txt fájlt, aminél a példa legyen:  proba.txt VALAMI=
-// ezt leforditom lengyel formává:
+// Peti, kapok .txt fájlt, aminél a példa legyen:  proba.txt VALAMI=a^b+(c+d)*a-e
+// ezt leforditom lengyel formává:  lengyel.txt ab^cd+a*+e-
 
 }
 
 void regiszteres ()
 {
-//Áron kapja a lengyel formát:
+//Áron kapja a lengyel formát: lengyel.txt ab^cd+a*+e-
 //leforditja regiszteresre:
 }
 
@@ -20,8 +20,7 @@ void gepikod ()
 
 {
 //Laci a kapott gépi kódot, kiszámolja:
-
-//eredmény , adott példákkal:
+//eredmény , adott példákkal:  a=1; b=2; c=3; d=4; e=5;   erre lesz VALAMI=4
 }
 
 
