4add830554a31b9e05d9053bd3a5115f9bd401ee
nemtomki <34485128+nemtomki@users.noreply.github.com>
2017-12-15 10:07:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
+#include <vector>
+#include <struct>
+#include <string>
+using namespace std;
 struct szoveg {
 string fnev;
  stack<string> lkod;
  string gkod;
 } ;
-using namespace std;
-
-void lengyel ()
+void lengyel (szoveg &pelda)
 {
 // Peti, kapok .txt fájlt, aminél a példa legyen:  proba.txt VALAMI=a^b+(c+d)*a-e
-// ezt leforditom lengyel formává:  lengyel.txt ab^cd+a*+e-
+// ezt leforditom lengyel formává:  példa.lkod={VALAMI;-e+*a+dc^ba}
 
 }
 
-void regiszteres ()
+void regiszteres (szoveg &pelda)
 {
 //Áron kapja a lengyel formát: lengyel.txt ab^cd+a*+e-
 //leforditja regiszteresre:
 }
 
 
-void gepikod ()
+void gepikod (szoveg &pelda)
 
 {
 //Laci a kapott gépi kódot, kiszámolja:
-//eredmény , adott példákkal:  a=1; b=2; c=3; d=4; e=5;   erre lesz VALAMI=4
+//eredmény , adott példákkal:  a=1; b=2; c=3; d=4; e=5;   erre lesz VALAMI=4, gepivel meg simával is
 }
 
 
@@ -32,8 +34,8 @@ int main()
 {
     szoveg pelda;
     pelda.fnev=''proba.txt'';
-    lengyel();
-    regiszteres();
-    gepikod();
+    lengyel(pelda);
+    regiszteres(pelda);
+    gepikod(pelda);
     return 0;
 }
